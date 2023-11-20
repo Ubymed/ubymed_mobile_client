a246966cc09fcc90d4b524b4ef306d458b3cf2f2
Ubymed <marioandf@gmail.com>
2023-11-19 18:37:58 -0600

    updated

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -25,29 +25,22 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-          headerRight: () => (
-            <Link href="/modal" asChild>
-              <Pressable>
-                {({ pressed }) => (
-                  <FontAwesome
-                    name="info-circle"
-                    size={25}
-                    color={Colors[colorScheme ?? 'light'].text}
-                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                  />
-                )}
-              </Pressable>
-            </Link>
-          ),
+          title: 'Inicio',
+          tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />,
         }}
       />
       <Tabs.Screen
-        name="two"
+        name="ordenes"
         options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          title: 'Órdenes',
+          tabBarIcon: ({ color }) => <TabBarIcon name="file" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="perfil"
+        options={{
+          title: 'Perfil',
+          tabBarIcon: ({ color }) => <TabBarIcon name="user" color={color} />,
         }}
       />
     </Tabs>
--- components/SectionHeader.tsx
+++ components/SectionHeader.tsx
@@ -3,7 +3,7 @@ import { Text, View } from './Themed';
 
 export const SectionHeader = ({ title }: { title: string }) => {
     return (
-      <View style={{ marginBottom: 10 }}>
+      <View style={{ marginTop: 20, marginBottom: 10 }}>
         <Text style={{ fontSize: 24, fontWeight: 'bold' }}>{title}</Text>
       </View>
     );
